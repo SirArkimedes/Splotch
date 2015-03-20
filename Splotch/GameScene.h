8b1950c986bd52d335ce359d5275a1c622caa396
Andrew Robinson <SirArkimedes@gmail.com>
2015-03-20 11:50:51 -0600

    Initial Commit

---
+++ Splotch/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Splotch
+//
+
+//  Copyright (c) 2015 Andrew Robinson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Splotch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Splotch
+//
+//  Created by Andrew Robinson on 3/20/15.
+//  Copyright (c) 2015 Andrew Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
