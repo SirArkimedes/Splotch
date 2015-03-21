cfbcff96432f465fbea0651b03556369c344fb5b
Andrew Robinson <SirArkimedes@gmail.com>
2015-03-20 22:13:06 -0500

    Detect collisions

--- Splotch/GameScene.m
+++ Splotch/GameScene.m
@@ -8,6 +8,13 @@
 
 #import "GameScene.h"
 
+typedef enum {
+    
+    heroCollider,
+    wallCollider
+    
+} collsions;
+
 @interface GameScene ()
 
 @property (strong, nonatomic) SKSpriteNode *hero;
@@ -18,6 +25,11 @@
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
+    
+    // Collisions
+    self.physicsWorld.contactDelegate = self;
+    
+    self.physicsWorld.gravity = CGVectorMake(0,0);
     
     // Setup touches
     UISwipeGestureRecognizer * swipeLeft= [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeLeft)];
@@ -33,7 +45,10 @@
     
     hero.position = CGPointMake(self.size.width/2, self.size.height/4);
     hero.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:hero.size];
-    hero.physicsBody.affectedByGravity = FALSE;
+//    hero.physicsBody.affectedByGravity = FALSE;
+    hero.physicsBody.categoryBitMask = heroCollider;
+    hero.physicsBody.collisionBitMask = heroCollider | wallCollider;
+    hero.physicsBody.contactTestBitMask = wallCollider | wallCollider;
     
     self.hero = hero;
     [self addChild:hero];
@@ -42,28 +57,55 @@
     SKSpriteNode *spriteWallLeft = [SKSpriteNode spriteNodeWithColor:[UIColor yellowColor] size:CGSizeMake(40, 100)];
     spriteWallLeft.position = CGPointMake(0, self.size.height/4);
     spriteWallLeft.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:spriteWallLeft.size];
-    spriteWallLeft.physicsBody.affectedByGravity = FALSE;
+//    spriteWallLeft.physicsBody.affectedByGravity = FALSE;
     spriteWallLeft.physicsBody.dynamic = NO;
+    spriteWallLeft.physicsBody.categoryBitMask = wallCollider;
+    spriteWallLeft.physicsBody.collisionBitMask = wallCollider | heroCollider;
+    spriteWallLeft.physicsBody.contactTestBitMask = heroCollider;
     [self addChild:spriteWallLeft];
     
     SKSpriteNode *spriteWallRight = [SKSpriteNode spriteNodeWithColor:[UIColor yellowColor] size:CGSizeMake(40, 100)];
     spriteWallRight.position = CGPointMake(self.frame.size.width, self.size.height/4);
     spriteWallRight.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:spriteWallRight.size];
-    spriteWallRight.physicsBody.affectedByGravity = FALSE;
+//    spriteWallRight.physicsBody.affectedByGravity = FALSE;
     spriteWallRight.physicsBody.dynamic = NO;
+    spriteWallRight.physicsBody.categoryBitMask = wallCollider;
+    spriteWallRight.physicsBody.collisionBitMask = wallCollider | heroCollider;
+    spriteWallRight.physicsBody.contactTestBitMask = heroCollider;
     [self addChild:spriteWallRight];
     
 //    self.physicsBody = [SKPhysicsBody ]
 }
 
+- (void)didBeginContact:(SKPhysicsContact *)contact {
+    
+    if (contact.bodyA.categoryBitMask == heroCollider || contact.bodyB.categoryBitMask == wallCollider) {
+        NSLog(@"Something collided");
+        
+//        contact.bodyA.dynamic = NO;
+//        self.hero.position = CGPointMake(self.hero.position.x, self.hero.position.y);
+    }
+    
+}
+
 - (void)swipeLeft {
     NSLog(@"Did Swipe Left");
     [self.hero.physicsBody applyImpulse:CGVectorMake(-10, 0)];
+    
+    if (self.hero.physicsBody.dynamic == NO) {
+        self.hero.physicsBody.dynamic = YES;
+    }
+    
 }
 
 - (void)swipeRight {
     NSLog(@"Did Swipe Right");
     [self.hero.physicsBody applyImpulse:CGVectorMake(10, 0)];
+    
+    if (self.hero.physicsBody.dynamic == NO) {
+        self.hero.physicsBody.dynamic = YES;
+    }
+    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
